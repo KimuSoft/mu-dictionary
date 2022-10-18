0b06529a5a5c8ca9420b1bca1ebf5c5947f24b09
Kimu-Nowchira <keleekini@gmail.com>
2022-10-18 12:44:54 +0900

    feat: station converting script

--- server/src/script/kric/index.ts
+++ server/src/script/kric/index.ts
@@ -5,16 +5,28 @@
   ※ xlsx 파일을 다운받아주세요
  */
 
-import { IWord, WordClass } from "../../types"
+import { IWord } from "../../types"
 import xlsx from "node-xlsx"
 import path from "path"
 import fs from "fs"
 
 const fileName = "전체_도시철도역사정보_20221007.xlsx"
 
+// 역사번호, 역사명, 노선번호, 노선명, 영어역사명, 한자역사명, 환승역구분, 운영기관명, 역사도로명주소
+interface Station {
+  code: string
+  name: string
+  lineNumber: string
+  lineName: string
+  englishName: string
+  chineseName: string
+  transferStation: string
+  operatingAgency: string
+  address: string
+}
+
 const convert = (fileName: string): IWord[] => {
   console.info(`Start to parse: ${fileName}`)
-
   const workSheetsFromFile = xlsx.parse(path.join(__dirname, fileName))
 
   console.info(`Start to convert: ${fileName}`)
@@ -26,24 +38,51 @@ const convert = (fileName: string): IWord[] => {
     index++
     if (index === 1) continue
 
-    try {
-      // if (!checkWordCondition(word.name)) continue
-      // words.push({
-      //   name: convertName(seriesName),
-      //   simpleName: getSimpleName(seriesName),
-      //   wordClass: WordClass.Noun,
-      //   definition: convertSeriesDefinition(foodData),
-      //   tags: ["food"],
-      //   reference: "foodSafetyKorea",
-      // })
-    } catch (error) {
-      console.error(error)
-      console.warn(`Failed to parse: ${row}`)
-    }
+    let name = row[1]
+
+    const subNameReg = /\((.+)\)/
+    if (subNameReg.test(row[1]))
+      try {
+        // 부역명 검사를 해야 함!
+        /*
+      명 덕
+(2·28민주운동기념회관)
+       */
+        const station: Station = {
+          code: row[0],
+          name: convertName(row[1]),
+          lineNumber: row[2],
+          lineName: row[3],
+          englishName: row[4],
+          chineseName: row[5],
+          transferStation: row[6],
+          operatingAgency: row[11],
+          address: row[12],
+        }
+        // if (!checkWordCondition(word.name)) continue
+        // words.push({
+        //   name: convertName(seriesName),
+        //   simpleName: getSimpleName(seriesName),
+        //   wordClass: WordClass.Noun,
+        //   definition: convertSeriesDefinition(foodData),
+        //   tags: ["food"],
+        //   reference: "foodSafetyKorea",
+        // })
+      } catch (error) {
+        console.error(error)
+        console.warn(`Failed to parse: ${row}`)
+      }
   }
 
   console.info(`Finish to convert: ${fileName}`)
   return words
+}
+
+const convertName = (name: string) => {
+  name = name.replace(/\(.*\)/g, "").replace(/\s/g, "")
+  if (!name.endsWith("역")) name += "역"
+
+  return name
 }
 
 const result = convert(fileName)
