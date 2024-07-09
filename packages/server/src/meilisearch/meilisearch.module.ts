b23c66f2a1af927dddde56fd75f80e0ff165d44a
keleekini@gmail.com <keleekini@gmail.com>
2024-07-09 12:50:23 +0900

    fix: 버그수정

--- packages/server/src/meilisearch/meilisearch.module.ts
+++ packages/server/src/meilisearch/meilisearch.module.ts
@@ -3,10 +3,18 @@ import { MeilisearchController } from './meilisearch.controller';
 import { MeilisearchService } from './meilisearch.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WordEntity } from '../words/word.entity';
+import { SYNC_WORD_REPOSITORY } from './repositories/sync-word.repository';
+import { SyncWordMeilisearchRepository } from './implements/sync-word-meilisearch.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([WordEntity])],
   controllers: [MeilisearchController],
-  providers: [MeilisearchService],
+  providers: [
+    MeilisearchService,
+    {
+      provide: SYNC_WORD_REPOSITORY,
+      useValue: SyncWordMeilisearchRepository,
+    },
+  ],
 })
 export class MeilisearchModule {}
