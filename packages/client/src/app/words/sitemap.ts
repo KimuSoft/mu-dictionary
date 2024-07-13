a45a5af5bf0d05dc91810f4f4785a7780d5437f3
keleekini@gmail.com <keleekini@gmail.com>
2024-07-13 18:32:31 +0900

    fix: sitemap issue

--- packages/client/src/app/words/sitemap.ts
+++ packages/client/src/app/words/sitemap.ts
@@ -2,12 +2,22 @@ import { MetadataRoute } from "next"
 import { fetchInitials } from "@/api/actions/fetchInitials"
 import { fetchWords } from "@/api/actions/fetchWords"
 import { FindMode } from "mudict-api-types"
+import { cache } from "react"
+
+export const dynamic = "force-dynamic"
+
+const fetchInitialsCache = cache(fetchInitials)
 
 export const generateSitemaps = async () => {
-  const initialData = await fetchInitials()
+  try {
+    const initialData = await fetchInitialsCache()
 
-  // initialData를 [{ㅇ: 100}, {ㅈ: 200}, {ㄱ: 300}] 형식으로 변환해서 리턴
-  return initialData.map((data) => ({ [data.initial]: data.count }))
+    // initialData를 [{ㅇ: 100}, {ㅈ: 200}, {ㄱ: 300}] 형식으로 변환해서 리턴
+    return initialData.map((data) => ({ [data.initial]: data.count }))
+  } catch (e) {
+    console.warn("Failed to fetch initials")
+    return []
+  }
 }
 
 const sitemap = async ({
